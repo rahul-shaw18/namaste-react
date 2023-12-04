04f338adb6fb8384e02d93bc0425ea3fd5c3d686
Rahul Shaw <rahul.shaw505@gmail.com>
2023-12-04 12:51:54 +0530

    project completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 const Contact = () => {
     return (
-        <h1>Contact</h1>
+        <div>
+            <h1 className="font-bold text-3xl p-4 m-4">Contact</h1>
+            <form action="">
+                <input className="border border-black p-2 m-2" type="text" name="" id="" />
+                <input className="border border-black p-2 m-2" type="text" name="" id="" />
+                <button className="border border-black p-2 m-2 rounded-lg bg-slate-300">Sumbit</button>
+            </form>
+        </div>
     )
 }
 
--- src/utils/useRestaurantList.js
+++ src/utils/useRestaurantList.js
@@ -12,7 +12,7 @@ const useRestaurantList = () => {
 
     const fetchData = async () => {
         const data = await fetch(RESTAURANT_API)
-        const json = await data.json()
+        const json = await data?.json()
         setRestaurantList(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
